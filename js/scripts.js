4d78f068087874dd44ba59dcb039ab99aa8e9e44
Razorink <stephenseymour98@gmail.com>
2019-10-27 05:19:20 -0400

    UI changes

--- js/scripts.js
+++ js/scripts.js
@@ -88,17 +88,17 @@ function getCandidate(result, owner){
         if (result[i].office === owner){
             if (result[i].party === "democrat")
             {
-                document.getElementById("c").innerHTML+= "<div class='col-lg-4 col-md-6 mb-4'> <div class='card h-100'> <a href='./candidatePage.html'><img class='card-img-top' src='https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778&w=1400&h=950' alt=''></a><div class='card-body'><h4 class='card-title'><a href='#'>"+ result[i].candidate +"</a></h4><p class='card-text'><button type='button' class='btn btn-outline-primary'>Democrat</button></p></div><div class='card-footer findMoreButton'><a type='button' href='../pages/candidatePage.html' class='btn btn-primary'>Learn More</a></div></div></div>";
+                document.getElementById("c").innerHTML+= "<div class='col-lg-4 col-md-6 mb-4'> <div class='card h-100'> <a href='./candidatePage.html'><img class='card-img-top' src='https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778&w=1400&h=950' alt=''></a><div class='card-body'><h4 class='card-title'>"+ result[i].candidate +"</a></h4><p class='card-text'><button type='button' class='btn btn-outline-primary'>Democrat</button></p></div><div class='card-footer findMoreButton'><a type='button' href='../pages/candidatePage.html' class='btn btn-primary'>Learn More</a></div></div></div>";
 
             }
             else if(result[i].party === "republican")
             {
-                document.getElementById("c").innerHTML+= "<div class='col-lg-4 col-md-6 mb-4'> <div class='card h-100'> <a href='./candidatePage.html'><img class='card-img-top' src='https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778&w=1400&h=950' alt=''></a><div class='card-body'><h4 class='card-title'><a href='#'>"+ result[i].candidate +"</a></h4><p class='card-text'><button type='button' class='btn btn-outline-danger'>Republican</button></p></div><div class='card-footer findMoreButton'><a type='button' href='../pages/candidatePage.html' class='btn btn-primary'>Learn More</a></div></div></div>";
+                document.getElementById("c").innerHTML+= "<div class='col-lg-4 col-md-6 mb-4'> <div class='card h-100'> <a href='./candidatePage.html'><img class='card-img-top' src='https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778&w=1400&h=950' alt=''></a><div class='card-body'><h4 class='card-title'>"+ result[i].candidate +"</a></h4><p class='card-text'><button type='button' class='btn btn-outline-danger'>Republican</button></p></div><div class='card-footer findMoreButton'><a type='button' href='../pages/candidatePage.html' class='btn btn-primary'>Learn More</a></div></div></div>";
 
             }
             else
             {
-                document.getElementById("c").innerHTML+= "<div class='col-lg-4 col-md-6 mb-4'> <div class='card h-100'> <a href='./candidatePage.html'><img class='card-img-top' src='https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778&w=1400&h=950' alt=''></a><div class='card-body'><h4 class='card-title'><a href='#'>"+ result[i].candidate +"</a></h4><p class='card-text'><button type='button' class='btn btn-outline-warning'>Independent</button></p></div><div class='card-footer findMoreButton'><a type='button' href='../pages/candidatePage.html' class='btn btn-primary'>Learn More</a></div></div></div>";
+                document.getElementById("c").innerHTML+= "<div class='col-lg-4 col-md-6 mb-4'> <div class='card h-100'> <a href='./candidatePage.html'><img class='card-img-top' src='https://image.cnbcfm.com/api/v1/image/104656161-GettyImages-688156110.jpg?v=1532563778&w=1400&h=950' alt=''></a><div class='card-body'><h4 class='card-title'>"+ result[i].candidate +"</a></h4><p class='card-text'><button type='button' class='btn btn-outline-warning'>Independent</button></p></div><div class='card-footer findMoreButton'><a type='button' href='../pages/candidatePage.html' class='btn btn-primary'>Learn More</a></div></div></div>";
 
             }
             
